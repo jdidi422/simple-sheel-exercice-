1730740bf31aa25ba584c1e64f2843eef647e9ca
jdidi422 <rassiljdidi3@gmail.com>
2025-01-03 13:24:16 +0100

    pid,pid

---
+++ pid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints the process ID and parent process ID
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+    pid_t pid, ppid;
+
+    pid = getpid();
+    ppid = getppid();
+
+    printf("pid=%d, parent id=%d\n", pid, ppid);
+
+    return (0);
+}
